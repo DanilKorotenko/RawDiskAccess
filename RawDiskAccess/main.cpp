3a431567afa8d70c57f526489e4f36f5125a2923
Danil Korotenko <danil.korotenko@gmail.com>
2026-07-06 17:21:45 +0300

    Initial commit.

---
+++ RawDiskAccess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RawDiskAccess
+//
+//  Created by Danil Korotenko on 7/6/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
